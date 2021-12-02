9a7cef65bcc0fb43b578eb481e5da982072b8a28
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2021-12-01 22:04:34 -0600

    day 1

--- src/day01.zig
+++ src/day01.zig
@@ -12,7 +12,42 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day01.txt");
 
 pub fn main() !void {
-
+    // create list
+    var list = std.ArrayList(u32).init(gpa);
+    defer list.deinit();
+    
+    // parse data and append to list
+    {
+        var iter = split(u8, data, "\n");
+        while(iter.next()) |s| {
+            const n = parseInt(u32, s, 10) catch continue;
+            try list.append(n);
+        }
+    }
+    
+    { // Part 1
+        var count: u32 = 0;
+        for (list.items) |n, index| {
+            if (index >= 1) {
+                const n1 = list.items[index - 1];
+                if (n > n1) count += 1;
+            }
+        }
+        print("{}\n", .{count});
+    }
+    
+    { // Part 2
+        var count: u32 = 0;
+        for (list.items) |n, index| {
+            if (index >= 3) {
+                const n1 = list.items[index - 1];
+                const n2 = list.items[index - 2];
+                const n3 = list.items[index - 3];
+                if (n+n1+n2 > n1+n2+n3) count += 1;
+            }
+        }
+        print("{}\n", .{count});
+    }
 }
 
 // Useful stdlib functions
