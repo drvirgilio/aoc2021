0e66385f0c0cf6ea4b612d26bce308e30a9b7ffe
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2021-12-05 00:45:15 -0600

    day 5

--- src/day05.zig
+++ src/day05.zig
@@ -11,7 +11,103 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day05.txt");
 
+const Line = struct {
+    x1: u32,
+    y1: u32,
+    x2: u32,
+    y2: u32,
+};
+
 pub fn main() !void {
+    const lines: []Line = blk: {
+        var iter_lines = tokenize(u8, data, "\n\r");
+        var list = std.ArrayList(Line).init(gpa);
+        while (iter_lines.next()) |s| {
+            var iter_nums = tokenize(u8, s, ", ->");
+            var line: Line = undefined;
+            line.x1 = try parseInt(u32, iter_nums.next().?, 10);
+            line.y1 = try parseInt(u32, iter_nums.next().?, 10);
+            line.x2 = try parseInt(u32, iter_nums.next().?, 10);
+            line.y2 = try parseInt(u32, iter_nums.next().?, 10);
+            try list.append(line);
+        }
+        break :blk list.toOwnedSlice();
+    };
+    
+    // only consider lines where x1==x2 or y1==y2
+    const axial_lines: []Line = blk: {
+        var list = std.ArrayList(Line).init(gpa);
+        for (lines) |line| {
+            if (line.x1==line.x2 or line.y1==line.y2) {
+                try list.append(line);
+            }
+        }
+        break :blk list.toOwnedSlice();
+    };
+    
+    { // Part 1
+        var coverage = [_][991]u8{[_]u8{0}**991}**991;
+        for (axial_lines) |line| {
+            if (line.x1 == line.x2) {
+                var y: usize = min(line.y1, line.y2);
+                while (y <= max(line.y1, line.y2)) : (y += 1) {
+                    coverage[line.x1][y] += 1;
+                }
+            } else if (line.y1 == line.y2) {
+                var x: usize = min(line.x1, line.x2);
+                while (x <= max(line.x1, line.x2)) : (x += 1) {
+                    coverage[x][line.y1] += 1;
+                }
+            } else unreachable;
+        }
+        var count: u32 = 0;
+        for (coverage) |row| {
+            for (row) |val| {
+                if (val >= 2) {
+                    count += 1;
+                }
+            }
+        }
+        print("{d}\n", .{count});
+    }
+    
+    { // Part 2
+        var coverage = [_][991]u8{[_]u8{0}**991}**991;
+        for (lines) |line| {
+             if (line.x1 == line.x2) {
+                var y: usize = min(line.y1, line.y2);
+                while (y <= max(line.y1, line.y2)) : (y += 1) {
+                    coverage[line.x1][y] += 1;
+                }
+            } else if (line.y1 == line.y2) {
+                var x: usize = min(line.x1, line.x2);
+                while (x <= max(line.x1, line.x2)) : (x += 1) {
+                    coverage[x][line.y1] += 1;
+                }
+            } else {
+                var x: usize = line.x1;
+                var y: usize = line.y1;
+                while (true) {
+                    coverage[x][y] += 1;
+                    if (x == line.x2) {
+                        assert(y == line.y2);
+                        break;
+                    }
+                    x = if (line.x1 < line.x2) x+1 else x-1;
+                    y = if (line.y1 < line.y2) y+1 else y-1;
+                }
+            }
+        }
+        var count: u32 = 0;
+        for (coverage) |row| {
+            for (row) |val| {
+                if (val >= 2) {
+                    count += 1;
+                }
+            }
+        }
+        print("{d}\n", .{count});
+    }
     
 }
 
