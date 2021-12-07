eea74d9cfdf5c245e90402d53ea1632bf9149fc8
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2021-12-07 09:17:20 -0600

    day 7

--- src/day07.zig
+++ src/day07.zig
@@ -10,9 +10,60 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day07.txt");
+//const data = "16,1,2,0,4,2,7,1,2,14";
 
 pub fn main() !void {
+    const input: []u32 = blk: {
+        var iter = tokenize(u8, data, ",\r\n");
+        var list = std.ArrayList(u32).init(gpa);
+        while (iter.next()) |s| {
+            const n = try parseInt(u32, s, 10);
+            try list.append(n);
+        }
+        break :blk list.toOwnedSlice();
+    };
     
+    { // part 1
+        // find initial upper and lower bounds
+        var low: u32 = comptime std.math.maxInt(u32);
+        var high: u32 = 0;
+        for (input) |n| {
+            if (n > high) high = n;
+            if (n < low) low = n;
+        }
+        
+        var mincost: u32 = std.math.maxInt(u32);
+        var testpoint = low; while (testpoint <= high) : (testpoint += 1) {
+            var cost: u32 = 0;
+            for (input) |n| {
+                cost += if (testpoint < n) (n - testpoint) else (testpoint - n);
+            }
+            if (cost < mincost) mincost = cost;
+        }
+        
+        print("{}\n", .{mincost});
+    }
+    { // part 2
+        // find initial upper and lower bounds
+        var low: u32 = comptime std.math.maxInt(u32);
+        var high: u32 = 0;
+        for (input) |n| {
+            if (n > high) high = n;
+            if (n < low) low = n;
+        }
+        
+        var mincost: u32 = std.math.maxInt(u32);
+        var testpoint = low; while (testpoint <= high) : (testpoint += 1) {
+            var cost: u32 = 0;
+            for (input) |n| {
+                const d = if (testpoint < n) (n - testpoint) else (testpoint - n);
+                cost += d*(d+1)/2;
+            }
+            if (cost < mincost) mincost = cost;
+        }
+        
+        print("{}\n", .{mincost});
+    }
 }
 
 // Useful stdlib functions
