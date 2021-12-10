23d20500f87b3f840e7f6d63500f220f7a6f4c10
David Virgilio <5187363+drvirgilio@users.noreply.github.com>
2021-12-10 11:58:41 -0600

    day 10 part 1

--- src/day10.zig
+++ src/day10.zig
@@ -12,7 +12,80 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day10.txt");
 
 pub fn main() !void {
+    const input: [][]u8 = blk: {
+        var list = std.ArrayList([]u8).init(gpa);
+        var iter = tokenize(u8, data, "\r\n");
+        while (iter.next()) |line| {
+            var list2 = std.ArrayList(u8).init(gpa);
+            for (line) |c| {
+                try list2.append(c);
+            }
+            try list.append(list2.toOwnedSlice());
+        }
+        break :blk list.toOwnedSlice();
+    };
     
+    { // part 1
+        var score: u32 = 0;
+        for (input) |line, line_index| {
+            var stack = std.ArrayList(u8).init(gpa);
+            const result: Result = for (line) |c, col_index| {
+                switch(c) {
+                    '(', '[', '{', '<' => try stack.append(c),
+                    ')', ']', '}', '>' => {
+                        const last_or_null = stack.popOrNull();
+                        if (last_or_null) |last| {
+                            if (isMatch(last, c)) {
+                                continue;
+                            } else {
+                                break Result{.corrupted = c};
+                            }
+                        } else {
+                            break Result.incomplete;
+                        }
+                    },
+                    else => {
+                        print("{d}:{s} :: {d}:{c}\n", .{line_index, line, col_index, c});
+                        unreachable;
+                    },
+                }
+            } else blk: {
+                break :blk Result.complete;
+            };
+            
+            switch (result) {
+                Result.complete => {},
+                Result.incomplete => {},
+                Result.corrupted => |c| switch (c) {
+                    ')' => score += 3,
+                    ']' => score += 57,
+                    '}' => score += 1197,
+                    '>' => score += 25137,
+                    else => unreachable,
+                }
+            }
+            
+        }
+        
+        print("{d}\n", .{score});
+    }
+}
+
+const Result = union(enum) {
+    complete: void,
+    incomplete: void,
+    corrupted: u8,
+};
+
+fn isMatch(fst: u8, snd: u8) bool {
+    const expected_snd: u8 = switch(fst) {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        else => unreachable,
+    };
+    return if (snd == expected_snd) true else false;
 }
 
 // Useful stdlib functions
